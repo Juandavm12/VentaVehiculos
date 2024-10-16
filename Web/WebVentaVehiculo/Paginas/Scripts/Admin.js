3699dbf66643234a6242df571dbb0867d50c3e10
Paucq <calderonpaula781@gmail.com>
2024-10-16 16:20:18 -0500

    Js: Admin, Vehiculo, Venta

--- Web/WebVentaVehiculo/Paginas/Scripts/Admin.js
+++ Web/WebVentaVehiculo/Paginas/Scripts/Admin.js
@@ -1,4 +1,42 @@
-﻿jQuery(function () {
-    //Registrar los botones para responder al evento click
-    $("#dvMenu").load("../Paginas/Menu.html")
-});
+﻿class Admin {
+    constructor(Documento, Nombre, Apellido, Direccion, Correo, Telefono, FechaNacimiento, Cargo) {
+
+        this.Documento = Documento;
+        this.Nombre = Nombre;
+        this.Apellido = Apellido;
+        this.Direccion = Direccion;
+        this.Correo = Correo;
+        this.Telefono = Telefono;
+        this.FechaNacimiento = FechaNacimiento;
+        this.Cargo = Cargo;
+    }
+}
+
+async function Execute(Method, Function) {
+    const admin = new Admin($("#txtDocumento").val(), $("#txtNombre").val(), $("#txtApellido").val(), $("#txtDireccion").val(),
+        $("#txtCorreo").val(), $("#txtTelefono").val(), $("#txtFechaNacimiento").val(), $("#txtCargo").val());
+    let URL = "https://localhost:44337/api/Admins/" + Function;
+    ExecuteCommandService(Method, URL, admin);
+}
+
+async function BuscarAdmin() {
+    let Documento = $("#txtDocumento").val();
+    URL = "https://localhost:44337/api/Admins/AdminxDocumento?Documento=" + Documento;
+
+    //invoco el servicio generico 
+    const Admin = await SearchService(URL);
+
+    if (Admin != null) {
+
+        $("#txtNombre").val(Admin.Nombre);
+        $("#txtApellido").val(Admin.Apellido);
+        $("#txtDireccion").val(Admin.Direccion);
+        $("#txtCorreo").val(Admin.Correo);
+        $("#txtTelefono").val(Admin.Telefono);
+        $("#txtFechaNacimiento").val(Admin.FechaNacimiento.split('T')[0]);
+        $("#txtCargo").val(Admin.Cargo);
+    }
+    else {
+        $("#dvMensaje").html("No se encontro el Administrador");
+    }
+}
--- Web/WebVentaVehiculo/Paginas/Scripts/Vehiculo.js
+++ Web/WebVentaVehiculo/Paginas/Scripts/Vehiculo.js
@@ -1,1 +1,44 @@
 ﻿
+class Vehiculo {
+
+    constructor(CodTipoVehiculo, Placa, Marca, Modelo, Precio, Kilometraje, CodEstadoVehiculo) {
+
+        this.CodTipoVehiculo = CodTipoVehiculo;
+        this.Placa = Placa;
+        this.Marca = Marca;
+        this.Modelo = Modelo;
+        this.Precio = Precio;
+        this.Kilometraje = Kilometraje;
+        this.CodEstadoVehiculo = CodEstadoVehiculo; 
+
+    }
+}
+
+async function Execute(Method, Function) {
+    const vehiculo = new Vehiculo($("#txtCodTipoVehiculo").val(), $("#txtPlaca").val(), $("#txtMarca").val(), $("#txtModelo").val(),
+        $("#txtPrecio").val(), $("#txtKilometraje").val(), $("#txtCodEstadoVehiculo").val());
+    let URL = "https://localhost:44337/api/Vehiculos/" + Function;
+    ExecuteCommandService(Method, URL, vehiculo);
+}
+
+async function BuscarVehiculo() {
+    let Placa = $("#txtPlaca").val();
+    URL = "https://localhost:44337/api/Vehiculos/BuscarxPlaca?Placa=" + Placa;
+
+    //invoco el servicio generico 
+    const Vehiculo = await SearchService(URL);
+
+    if (Vehiculo != null) {
+
+        $("#txtCodTipoVehiculo").val(Vehiculo.CodTipoVehiculo);
+        $("#txtPlaca").val(Vehiculo.Placa);
+        $("#txtMarca").val(Vehiculo.Marca);
+        $("#txtModelo").val(Vehiculo.Modelo);
+        $("#txtPrecio").val(Vehiculo.Precio);
+        $("#txtKilometraje").val(Vehiculo.Kilometraje);
+        $("#txtCodEstadoVehiculo").val(Vehiculo.CodEstadoVehiculo);
+    }
+    else {
+        $("#dvMensaje").html("No se encontro el Vehiculo");
+    }
+}
