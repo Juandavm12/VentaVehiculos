b4b1c095648888eabed4475c14c9027fcefb9c07
Paucq <calderonpaula781@gmail.com>
2024-10-16 02:01:37 -0500

    JavaScript

---
+++ Web/WebVentaVehiculo/Paginas/Scripts/Comunes/CRUD.js
@@ -0,0 +1,45 @@
+﻿jQuery(function () {
+    //Registrar los botones para responder al evento click
+    $("#dvMenu").load("../Paginas/Menu.html")
+});
+
+async function ExecuteCommandService(Method, URlService, Object) {
+    const cliente = new Cliente($("#txtTipoCliente").val(), $("#txtDocumento").val(), $("#txtNombre").val(), $("#txtApellido").val(), $("txtFechaNacimiento").val(),
+        $("#txtDireccion").val(), $("#txtTelefono").val(), $("#txtCorreoElectronico").val());
+
+    try {
+        const Response = await fetch(URlService,
+            {
+                method: Method,
+                mode: "cors",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(Object)
+            });
+
+        const Result = await Response.json();
+        $("#dvMensaje").html(Result);
+    }
+    catch (error) {
+        $("#dvMensaje").html(error);
+    }
+}
+
+async function SearchService(URLService) {
+    let Documento = $("#txtDocumento").val();
+
+    try {
+        const Response = await fetch(URLService,
+            {
+                method: "GET",
+                mode: "cors",
+                headers: { "Content-Type": "application/json" }
+            });
+
+        const Result = await Response.json();
+
+        return Result; 
+    }
+    catch (error) {
+        $("#dvMensaje").html(error);
+    }
+}
--- Web/WebVentaVehiculo/Paginas/Scripts/Venta.js
+++ Web/WebVentaVehiculo/Paginas/Scripts/Venta.js
@@ -1,4 +1,5 @@
-﻿jQuery(function () {
-    //Registrar los botones para responder al evento click
-    $("#dvMenu").load("../Paginas/Menu.html")
-});
+﻿//class Venta {
+//    constructor() {
+
+//    }
+//}
