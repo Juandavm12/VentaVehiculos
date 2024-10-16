3e6c495d0d492b892758b35c88d5daf6dddffe81
Juandav12 <JuanDavid@JUANDAV12>
2024-10-16 14:30:25 -0500

    Correccion errores

--- Web/WebVentaVehiculo/Paginas/Scripts/Comunes/CRUD.js
+++ Web/WebVentaVehiculo/Paginas/Scripts/Comunes/CRUD.js
@@ -3,9 +3,9 @@
     $("#dvMenu").load("../Paginas/Menu.html")
 });
 
-async function ExecuteCommandService(Method, URlService, Object) {
+async function ExecuteCommandService(Method, URLService, Object) {
     try {
-        const Response = await fetch(URlService,
+        const Response = await fetch(URLService,
             {
                 method: Method,
                 mode: "cors",
@@ -22,7 +22,6 @@ async function ExecuteCommandService(Method, URlService, Object) {
 }
 
 async function SearchService(URLService) {
-    let Documento = $("#txtDocumento").val();
 
     try {
         const Response = await fetch(URLService,
@@ -35,6 +34,32 @@ async function SearchService(URLService) {
         const Result = await Response.json();
 
         return Result; 
+    }
+    catch (error) {
+        $("#dvMensaje").html(error);
+    }
+}
+
+//Llenar Combo desde un servicio
+async function LlenarComboxServicios(URLServicio, ComboLlenar) {
+    try {
+        const Respuesta = await fetch(URLServicio,
+            {
+                method: "GET",
+                mode: "cors",
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            });
+        const Rpta = await Respuesta.json();
+
+        //Se limpia el combo
+        $(ComboLlenar).empty();
+
+        //Se recorre un ciclo para llenar el select con la informacion
+        for (i = 0; i < Rpta.length; i++) {
+            $(ComboLlenar).append('<option value=' + Rpta[i].Documento + '>' + Rpta[i].Nombre + '</option>');
+        }
     }
     catch (error) {
         $("#dvMensaje").html(error);
