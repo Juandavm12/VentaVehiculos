a4645735add517e1d5ca0223f7f01b5f77fe59f6
Juandav12 <JuanDavid@JUANDAV12>
2024-11-17 14:44:57 -0500

    CalculoFactura

--- VentaVehiculos/Clases/clsFactura.cs
+++ VentaVehiculos/Clases/clsFactura.cs
@@ -17,6 +17,47 @@ namespace VentaVehiculos.Clases
         //Objeto de la tabla Cliente 
         public Cliente cliente { get; set; }
 
-        
+        public Factura BuscarFactura(int numero)
+        {
+            //We use lambda to take the object type we are using
+            return dbVentaVehiculos.Facturas.FirstOrDefault(x => x.Numero == numero);
+        }
+
+        public Cliente DescuentoTipoCliente(int id)
+        {
+            cliente = new Cliente();
+            cliente.IdTipoCliente = id;
+            CalculoFactura();
+            return cliente;
+        }
+
+        public void CalculoFactura()
+        {
+            factura = new Factura();
+            cliente = new Cliente();
+
+            switch (cliente.IdTipoCliente)
+            {
+                case 1:
+                    factura.Subtotal = factura.Cantidad + factura.VUnidad;
+                    factura.Descuento = Convert.ToInt32(factura.Subtotal * 0.05);
+                    factura.VTotal = factura.Subtotal - factura.Descuento;
+                    break;
+
+                case 2:
+                    factura.Subtotal = factura.Cantidad + factura.VUnidad;
+                    factura.Descuento = Convert.ToInt32(factura.Subtotal * 0.1);
+                    factura.VTotal = factura.Subtotal - factura.Descuento;
+                    break;
+
+                case 3:
+                    factura.Subtotal = factura.Cantidad + factura.VUnidad;
+                    factura.Descuento = Convert.ToInt32(factura.Subtotal * 0.15);
+                    factura.VTotal = factura.Subtotal - factura.Descuento;
+                    break;
+            }
+        }
+
+
     }
 }
